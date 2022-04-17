ae9617694bdf911f8faf2d5f03bb26064b0ab9ca
tvl83 <tvle83@gmail.com>
2022-04-17 10:55:23 -0500

    initial design. Using gravatar for profile icons using fio address.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Md5} from 'ts-md5/dist/md5';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector   : 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fiomarketplace';
+	title = 'FIO Domain Marketplace';
+
+	getGravatarURL(email: any) {
+		const md5 = new Md5();
+		const hash = md5.appendStr(String(email).trim().toLowerCase()).end();
+		return `https://www.gravatar.com/avatar/${hash}?d=retro`;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {StyleClassModule} from 'primeng/styleclass';
+import {RippleModule} from 'primeng/ripple';
+import {ButtonModule} from 'primeng/button';
+import {MenuModule} from "primeng/menu";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StyleClassModule,
+    RippleModule,
+    ButtonModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
