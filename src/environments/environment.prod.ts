fcdd2fa6fb95fb371be18d9c5323af05b4a8b1cb
tvl83 <tvle83@gmail.com>
2022-06-01 17:17:58 -0500

    fixed apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   walletHost: 'fio-mainnet.eosblocksmith.io',
-  apiUrl    : 'api-mainnet.fiomarketplace.com'
+  apiUrl    : 'api.fiomarketplace.com'
 };
